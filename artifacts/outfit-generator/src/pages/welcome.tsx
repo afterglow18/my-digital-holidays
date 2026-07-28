582e11362f2e89ffdba93ea9556a1bd2871e5b20
afterglow18 <afterglowtanning18@gmail.com>
2026-07-28 05:14:14 +0000

    Light switch rocker: change ON label to ENTER

--- artifacts/outfit-generator/src/pages/welcome.tsx
+++ artifacts/outfit-generator/src/pages/welcome.tsx
@@ -108,7 +108,7 @@ function LightSwitch({ onFlip, disabled }: { onFlip: () => void; disabled: boole
             letterSpacing: "0.18em",
             color: flipped ? "rgba(60,30,5,0.7)" : "rgba(255,255,255,0.18)",
           }}>
-            {flipped ? "ON" : "OFF"}
+            {flipped ? "ENTER" : "OFF"}
           </span>
         </motion.div>
       </div>
